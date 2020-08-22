1881f79ab089f0dda08de7b27938375edc769b0f
creative-buffer-9578 <cmuusha@gmail.com>
2020-08-22 02:53:09 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,7 +6,14 @@ function frameIt() {
   $('img').addClass("tasty")
 }
 
+function pressIt() {
+  if ($( "input" ).val() === "71") {
+    alert('d now');
+  }
+}
+
 $(document).ready(function() {
   $('p').on('click',getIt());
   $('img').on('load',frameIt());
+  $('form').on('keydown',pressIt());
 });
