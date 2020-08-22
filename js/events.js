aebc3bcc2030065343fee4767d3358b59b914676
creative-buffer-9578 <cmuusha@gmail.com>
2020-08-22 03:15:55 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,11 +8,16 @@ function frameIt() {
 
 function pressIt() {
   if ($( "input:first" ).val() === "G")
-    alert('G was pressed')
+    alert('G was pressed');
   }
+
+function submitIt() {
+    alert("Your form is going to be submitted now.");
+}
 
 $(document).ready(function() {
   $('p').on('click',getIt());
   $('img').on('load',frameIt());
   $('input').on('keydown',pressIt());
+  $('form').on('submit',submitIt());
 });
